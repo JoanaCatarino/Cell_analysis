5d5d679286aca985a555dd2521296382034d9820
Joana Catarino <joana.fa.catarino@gmail.com>
2023-10-31 14:32:04 +0100

    Created script to analyse Cfos expression based on Napari output file

---
+++ Cfos_analysis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Created by Joana Catarino
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import matplotlib as
+
+# Import data for a single animal
+data = pd.read_csv('C:\Users\JoanaCatarino\OneDrive - Karolinska Institutet\Dokument\Cfos analysis')
