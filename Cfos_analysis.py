6bf50997ae244036405e63446e25577de3064b02
Joana Catarino <joana.fa.catarino@gmail.com>
2023-11-01 23:03:12 +0100

    Add barplot

--- Cfos_analysis.py
+++ Cfos_analysis.py
@@ -11,17 +11,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # Import data for a single animal
-data = pd.read_csv('C:/Users/JoanaCatarino/OneDrive_KI/OneDrive - Karolinska Institutet/Skrivbordet/Joana/Cfos_analysis/708075_cells.csv')
+#data = pd.read_csv('C:/Users/JoanaCatarino/OneDrive_KI/OneDrive - Karolinska Institutet/Skrivbordet/Joana/Cfos_analysis/708075_cells.csv')  #For desktop
+data = pd.read_csv('/Users/joanacatarino/Desktop/708075_cells.csv') #For mac
+
 
 # Info about the experiment
 animal_id = 708075
-genotype = 'Rbp4_Cre_neg'
+genotype = 'Rbp4 Cre_neg'
+
 
 # Create a new data frame that excludes the cells that were found outside the brain slice = root 
 slice_data = data[~(data['acronym'] == 'root')]
 
-#Check if there is any data in between hemispheres and remove it from the dataset
+# Check if there are any cells located the middle of the two hemispheres and remove it from the dataset
 mid = slice_data[slice_data['ml_mm'] == 0]  
 slice_data = slice_data[~(slice_data['ml_mm'] == 0)]
 
@@ -38,11 +42,17 @@ slice_data = slice_data.reset_index(drop=True)
 right_cells = len(slice_data[slice_data['hemisphere'] == 'right']) #Total amount of cells in the right hemisphere
 left_cells = len(slice_data[slice_data['hemisphere'] == 'left']) #Total amount of cells in the left hemisphere
 
+x = ['Right Hemisphere', 'Left Hemisphere']
+y = [right_cells, left_cells] 
+color= ['#85BDA6', '#08605F'] # Select color for the bars, light green for right hemisphere and dark green for lost hemisphere
+
+fig, ax = plt.subplots(1,1, dpi= 500)
+ax.bar(x, y, width=0.1, color=color)
+ax.set(ylim=(0,10000))
+ax.set_title(f'#{animal_id}  {genotype}')
+plt.ylabel('Total number of cells')
+plt.show()
 
 
 
-
-
-ax = slice_data.plot.bar(x=(slice_data[slice_data['hemisphere']]), y=len(slice_data[slice_data['hemisphere']]), rot=0)
-
-
+    
