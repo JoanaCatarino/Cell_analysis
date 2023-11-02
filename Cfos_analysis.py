faaff9a8dde702fbe4eee5ecdc961a60d77218c5
Joana Catarino <joana.fa.catarino@gmail.com>
2023-11-02 22:50:37 +0100

    cell counting in PFC

--- Cfos_analysis.py
+++ Cfos_analysis.py
@@ -12,16 +12,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+#Import modules for stats
+from numpy.random import seed 
+from numpy.random import randn 
+from numpy.random import normal 
+from scipy.stats import ttest_ind
+
 
 # Import data for a single animal
-data = pd.read_csv('C:/Users/JoanaCatarino/OneDrive_KI/OneDrive - Karolinska Institutet/Skrivbordet/Joana/Cfos_analysis/708075_cells.csv')  #For desktop
-#data = pd.read_csv('/Users/joanacatarino/Desktop/708075_cells.csv') #For mac
-
+#data = pd.read_csv('C:/Users/JoanaCatarino/OneDrive_KI/OneDrive - Karolinska Institutet/Skrivbordet/Joana/Cfos_analysis/708075_cells.csv')  #For desktop
+data = pd.read_csv('/Users/joanacatarino/Desktop/708075_cells.csv') #For mac
 
 # Info about the experiment
 animal_id = 708075
 genotype = 'Rbp4 Cre_neg'
-
 
 # Create a new data frame that excludes the cells that were found outside the brain slice = root 
 slice_data = data[~(data['acronym'] == 'root')]
@@ -47,15 +51,92 @@ x = ['Right Hemisphere', 'Left Hemisphere']
 y = [right_cells, left_cells] 
 color= ['#85BDA6', '#08605F'] # Select color for the bars, light green for right hemisphere and dark green for left hemisphere
 
-fig, ax = plt.subplots(1,1, figsize=(3.8,5), dpi= 500)
+fig, ax = plt.subplots(1,1, figsize=(4,5), dpi= 500)
 ax.bar(x, y, width=0.4, color=color)
 ax.set(ylim=(0,10000))
-ax.set_title(f'#{animal_id}  {genotype}', fontsize=11, x=0.4 , y=1.02)
+ax.set_title(f'#{animal_id}  {genotype}', fontsize=11, fontweight='bold', x=0.4 , y=1.05)
 ax.bar_label(ax.containers[0], fontsize=8.5, color='#494949', padding=3)
 ax.margins(x=0.2)
-plt.ylabel('Total number of cells', fontsize=10)
+plt.ylabel('Total number of cells', fontsize=10, labelpad=8)
 plt.tight_layout()
 sns.despine()
 plt.show()
+
+#Add line to save fig
+
+#%%
+
+# Quantify total number of cells in PFC for each hemisphere
+    # PFC subregions to include: PL, ILA, ACAd, ACAv, ORBm, ORBl, ORBvl
+
+# Create a new column that only includes the name of the region without specifying the layer
+slice_data['region'], slice_data['layer'] = slice_data['name'].str.split(',', n=1).str
+
+# Get regions of interest (don't discriminate dorsal, ventral, medial, lateral, layers)
+PL_left = len(slice_data[slice_data['hemisphere'] == 'left']) and (slice_data[slice_data['region'] == 'Prelimbic area']) 
+ILA_left = len(slice_data[slice_data['hemisphere'] == 'left']) and (slice_data[slice_data['region'] == 'Infralimbic area']) 
+ACA_left = len(slice_data[slice_data['hemisphere'] == 'left']) and (slice_data[slice_data['region'] == 'Anterior cingulate area']) 
+ORB_left = len(slice_data[slice_data['hemisphere'] == 'left']) and (slice_data[slice_data['region'] == 'Orbital area']) 
+
+PL_right = len(slice_data[slice_data['hemisphere'] == 'right']) and (slice_data[slice_data['region'] == 'Prelimbic area']) 
+ILA_right = len(slice_data[slice_data['hemisphere'] == 'right']) and (slice_data[slice_data['region'] == 'Infralimbic area']) 
+ACA_right = len(slice_data[slice_data['hemisphere'] == 'right']) and (slice_data[slice_data['region'] == 'Anterior cingulate area']) 
+ORB_right = len(slice_data[slice_data['hemisphere'] == 'right']) and (slice_data[slice_data['region'] == 'Orbital area']) 
+
+'''
+PL_right2 = len(slice_data[slice_data['hemisphere'] == 'right'] and slice_data[slice_data['region'] == 'Prelimbic area'])
+
+'''
+
+# Get amount of cells in each region of interest
+PL_left_cells = PL_left[PL_left.columns[0]].count() 
+ILA_left_cells = ILA_left[ILA_left.columns[0]].count() 
+ACA_left_cells = ACA_left[ACA_left.columns[0]].count() 
+ORB_left_cells = ORB_left[ORB_left.columns[0]].count() 
+
+PL_right_cells = PL_right[PL_right.columns[0]].count() 
+ILA_right_cells = ILA_right[ILA_right.columns[0]].count() 
+ACA_right_cells = ACA_right[ACA_right.columns[0]].count() 
+ORB_right_cells = ORB_right[ORB_right.columns[0]].count() 
+
+# Get total amount of cells in PFC for each hemisphere
+PFC_left = PL_left_cells + ILA_left_cells + ACA_left_cells + ORB_left_cells
+PFC_right = PL_right_cells + ILA_right_cells + ACA_right_cells + ORB_right_cells
+
+# Plot
+
+x = ['PFC right', 'PFC left']
+y = [PFC_right, PFC_left] 
+color= ['#85BDA6', '#08605F'] # Select color for the bars, light green for right hemisphere and dark green for left hemisphere
+
+fig, ax = plt.subplots(1,1, figsize=(4,5), dpi= 500)
+ax.bar(x, y, width=0.4, color=color)
+ax.set(ylim=(0,10000))
+ax.set_title(f'#{animal_id}  {genotype}', fontsize=11, fontweight='bold', x=0.4 , y=1.05)
+ax.bar_label(ax.containers[0], fontsize=8.5, color='#494949', padding=3)
+ax.margins(x=0.2)
+plt.ylabel('Total number of cells', fontsize=10, labelpad=8)
+plt.tight_layout()
+sns.despine()
+plt.show()
+
+# fazer grafico com as barras de todo o cerebero e pfc sobrepostas
+
+#%%
+
+# Compare the number of cells between different PFC subregions for each hemisphere
+    # Regions to include: PL, ILA, ACAd, ACAv, ORBm, ORBl, ORBvl, Mos, Ald, Alv
+
+
+
+'''
+t_stat, p_value = ttest_ind(right_cells, left_cells) 
+print('T-statistic value: ', t_stat)  
+print('P-Value: ', p_value)
+'''
+
+
+
+
 
 
