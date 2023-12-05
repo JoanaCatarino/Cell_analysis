d571d25331eb6565397186c77537e91b28e2d635
Joana Catarino <joana.fa.catarino@gmail.com>
2023-12-05 13:14:43 +0100

    add specific columns for area, part and layer to make it easier to filter

--- Data_prep.py
+++ Data_prep.py
@@ -4,9 +4,10 @@ Created on Mon Nov 20 16:53:46 2023
 
 @author: JoanaCatarino
 
-Script to organize the data that Napari generates to be in a more user friendly form and to contain only the information needed for 
+Script to organize the data that Napari generates to be in a more user friendly format and to contain only the information needed for 
 the type of analysis we want
 
+IMP!!!! csv file needs to contain the injected side and the stimulated side 
 """
 
 import pandas as pd
@@ -15,15 +16,17 @@ import numpy as np
 import seaborn as sns
 
 # Import data for a single animal
-data = pd.read_csv('C:/Users/JoanaCatarino/OneDrive_KI/OneDrive - Karolinska Institutet/Skrivbordet/Joana/Cfos_analysis/714001/714001_cells.csv')  #For desktop
-
+data = pd.read_csv('C:/Users/JoanaCatarino/OneDrive_KI/OneDrive - Karolinska Institutet/Skrivbordet/Joana/Cfos_analysis/708075/708075_cells.csv')  #For desktop
 
 # CSV file with relevant infromation for experiment
-info = 
+info = pd.read_csv('C:/Users/JoanaCatarino/OneDrive_KI/OneDrive - Karolinska Institutet/Skrivbordet/Joana/Cfos_analysis/Experiment_info.csv', sep=';')
 
+# Remove the column 'Unnamed' because it is not giving any extra information
+data = data.drop(columns=['Unnamed: 0'])
 
-# Create a new data frame that excludes the cells that were found outside the brain slice = root 
+# Create a new data frame that excludes the cells that were found outside the brain slice = root and reset index
 slice_data = data[~(data['acronym'] == 'root')]
+slice_data = slice_data.reset_index(drop=True)
 
 # Check if there are any cells located the middle of the two hemispheres and remove it from the dataset
 mid = slice_data[slice_data['ml_mm'] == 0]  
@@ -33,10 +36,39 @@ slice_data = slice_data[~(slice_data['ml_mm'] == 0)]
 right_hemisphere = slice_data[slice_data['ml_mm'] > 0]
 left_hemisphere = slice_data[slice_data['ml_mm'] < 0]
 
-# Create a column showing which data is on each hemisphere and rearrange index
+# Create a column showing which data is on each hemisphere
 slice_data['hemisphere'] = np.where(slice_data['ml_mm'] > 0, 'right', 'left')
-slice_data = slice_data.drop(columns=['Unnamed: 0']) # This column contains the old index so we don't need it
-slice_data = slice_data.reset_index(drop=True)
+
+# To create a column with layers (teste again if this is working)
+region=[]
+
+for name in slice_data['name'].values:
+    region.append(name.split(',')[0])
+print(region)
+
+slice_data['region'] = region # Add column that only include regions to the dataframe
+
+# To create a column with the part of the regions where the cells are 
+part=[]
+
+for name in slice_data['name'].values:
+    #print(name.split(','))
+    if len(name.split(',')) == 3:
+        if name.split(',')[1].endswith('part'):
+            part.append(name.split(',')[1])
+        else:part.append(None)
+
+    elif len(name.split(',')) == 2:
+        if name.split(',')[-1].endswith('part'): # remove this to work a bit better
+            part.append(name.split(',')[-1])
+        else:
+            part.append(None)
+    else:
+        part.append(None)      
+print(part)
+
+slice_data['part'] = part
+  
 
 
 
@@ -51,3 +83,21 @@ slice_data['position'], slice_data['layer4real'] = slice_data['layer'].str.split
 #Add a column that only specifies the animal ID an drop the one with the obj name and file type (section_name)
 slice_data.insert(0, 'animal_id', animal_id) #Add new column
 slice_data = slice_data.drop(columns=['section_name']) # Drop old one
+
+
+
+for name in slice_data['name'].values:
+    #print(name.split(','))
+    if len(name.split(',')) == 3:
+        print(name.split(',')[-1])
+       
+        
+    elif len(name.split(',')) == 2:
+       if name.split(',')[-1].startswith('layer'):
+           layer.append(name.split(',')[-1])
+       else: 
+           layer.append(None)
+
+    else: 
+        layer.append(None)
+print(layer)
